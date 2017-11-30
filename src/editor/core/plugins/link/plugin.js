2fde7d3aa0c1745e432d96716f6451a1e36c5ebe
buddha87 <mail@jharrer.de>
2017-11-30 23:13:58 +0100

    Fix #1 invalid mark order code/strong Added linkify plugin

---
+++ src/editor/core/plugins/link/plugin.js
@@ -0,0 +1,56 @@
+import { Plugin } from 'prosemirror-state'
+import {Slice, Fragment} from "prosemirror-model"
+
+let linkPlugin = new Plugin({
+    props: {
+        transformPasted: (slice) => {
+            debugger;
+            let fragment = linkify(slice.content);
+            let node = fragment.content[0];
+            let test = node.type.validContent(node.content);
+            let testSlice = new Slice(linkify(slice.content), slice.openStart, slice.openEnd)
+            debugger;
+            return testSlice;
+        }
+    }
+});
+
+const HTTP_LINK_REGEX = /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,})/ig
+let linkify = function(fragment) {
+    var linkified = []
+    fragment.forEach(function(child){
+        if (child.isText) {
+            const text = child.text
+            var pos = 0, match
+
+            while (match = HTTP_LINK_REGEX.exec(text)) {
+                debugger;
+                var start = match.index
+                var end = start + match[0].length
+                var link = child.type.schema.marks['link']
+
+                // simply copy across the text from before the match
+                if (start > 0) {
+                    linkified.push(child.cut(pos, start))
+                }
+
+                const urlText = text.slice(start, end)
+                linkified.push(
+                    child.cut(start, end).mark(link.create({href: urlText}).addToSet(child.marks))
+                );
+                pos = end
+            }
+
+            // copy over whatever is left
+            if (pos < text.length) {
+                linkified.push(child.cut(pos))
+            }
+        } else {
+            linkified.push(child.copy(linkify(child.content)))
+        }
+    })
+
+    return Fragment.fromArray(linkified)
+}
+
+export {linkPlugin}
