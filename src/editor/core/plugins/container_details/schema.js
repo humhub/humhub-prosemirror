8f8584a2f9f564e18b273cc78e520ab1ddf1bc70
schlagmichdoch <52242352+schlagmichdoch@users.noreply.github.com>
2022-01-12 13:51:49 +0100

    working version without summary tags

--- src/editor/core/plugins/container_details/schema.js
+++ src/editor/core/plugins/container_details/schema.js
@@ -10,22 +10,18 @@ const schema = {
             defining: true,
             parseDOM: [{tag: "details"}],
             toDOM(node) {
-                return ["details", {open: ""}, 0]
+                return ["details", {open: ""}, /*["summary", node.attrs.summary], ["div", 0]*/ 0]
             },
             parseMarkdown: {
                 block: "container_details", getAttrs: function (tok) {
-                    console.log(tok.level)
                     return ({level: +tok.level});
                     // return ({summary: +tok.nodes.first().content});
                 }
             },
             toMarkdown: (state, node) => {
-                state.write(":::" + state.repeat(":", node.attrs.level) + " details " + node.attrs.summary + "\n\n");
-                // state.ensureNewLine();
+                state.write(":::" + state.repeat(":", 10 - node.attrs.level) + " details " + /*node.attrs.summary + */"\n\n");
                 state.renderContent(node)
-                // state.ensureNewLine();
-                state.write(":::" + state.repeat(":", node.attrs.level) + "\n\n");
-                // state.ensureNewLine();
+                state.write(":::" + state.repeat(":", 10 - node.attrs.level) + "\n\n");
                 state.closeBlock(node);
             }
         }
