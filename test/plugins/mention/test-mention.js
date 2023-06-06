231959a5c774ae62d7ffe54ad91efea345a63093
yuriimaz <nopangolier1@gmail.com>
2023-06-06 10:11:36 +0300

    Changed mention test

--- test/plugins/mention/test-mention.js
+++ test/plugins/mention/test-mention.js
@@ -22,9 +22,8 @@ describe("Plugin:mention", () => {
         const tempContainer = document.createElement('div');
         tempContainer.appendChild(clonedRange.cloneContents());
         const copiedText = tempContainer.textContent;
-        // console.log(clonedRange, clonedRange.cloneContents(), copiedText);
-
-        navigator.clipboard.writeText(copiedText);
+        return copiedText;
+        // navigator.clipboard.writeText(copiedText);
         // window.clipboardData.setData('text/html', copiedText);
     };
 
@@ -35,15 +34,14 @@ describe("Plugin:mention", () => {
 
         const parser = DOMParser.fromSchema(state.schema);
         const slice = parser.parseSlice(element);
-        // console.log(element, state.selection.anchor, slice.content);
         const tr = state.tr.insert(state.selection.anchor, slice.content);
 
         view.dispatch(tr);
     }
 
     it("test copy/paste text with mention", (done) => {
-        // const htmlContent = "<p>Mention <span data-mention-query=\"true\" style=\"color: #0078D7\">@John Doe</span></p>";
         const editor = initEditor();
+        // const htmlContent = "<p>Mention <span data-mention-query=\"true\" style=\"color: #0078D7\">@John Doe</span></p>";
         // insertHTML(editor.view, htmlContent);
 
         const view = editor.view;
@@ -51,22 +49,19 @@ describe("Plugin:mention", () => {
         pressKeyEnter();
 
         selectAllText(editor);
-        copySelection();
+        const text = copySelection();
 
-        navigator.clipboard.readText().then(text => {
-            console.log(text);
-            if (text) {
-                // Insert the text into the editor
-                const {tr} = view.state;
-                const {from, to} = view.state.selection;
-                tr.insertText(text, from, to);
-                view.dispatch(tr);
-            }
+        // navigator.clipboard.readText().then(text => {
+        // Insert the text into the editor
+        const {tr} = view.state;
+        const {from, to} = view.state.selection;
+        tr.insertText(text, from, to);
+        view.dispatch(tr);
 
-            expect(toHtml()).to.equal('<p>Mention @John Doe</p>');
-            expect(serialize()).to.equal('Mention @John Doe');
+        expect(toHtml()).to.equal('<p>Mention @John Doe</p><p>Mention @John Doe</p>');
+        expect(serialize()).to.equal('Mention @John Doe\n\nMention @John Doe');
 
-            done();
-        });
+        done();
+        // });
     });
 });
