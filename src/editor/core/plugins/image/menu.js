6d2a3044c53f153b813ba6eaed4e2dd2fa5c150f
yuriimaz <nopangolier1@gmail.com>
2023-06-26 11:22:29 +0300

    Fix #97: [Bug] Images duplicate on drag and drop

--- src/editor/core/plugins/image/menu.js
+++ src/editor/core/plugins/image/menu.js
@@ -2,11 +2,11 @@
  * @link https://www.humhub.org/
  * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
- *
  */
-import {MenuItem, canInsert, canInsertLink} from "../../menu/"
-import {TextField, openPrompt, SelectField} from "../../prompt"
-import {NodeSelection} from "prosemirror-state"
+
+import {MenuItem, canInsert, canInsertLink} from "../../menu/";
+import {TextField, openPrompt, SelectField} from "../../prompt";
+import {NodeSelection} from "prosemirror-state";
 import {validateHref} from "../../util/linkUtil";
 
 function insertImageItem(context) {
@@ -21,20 +21,20 @@ function insertImageItem(context) {
             if (state.selection instanceof NodeSelection && state.selection.node.type === context.schema.nodes.image) {
                 editNode(state.selection.node, context, view);
             } else {
-                promt(context.translate("Insert image"), context, null, view)
+                promt(context.translate("Insert image"), context, null, view);
             }
         }
     })
 }
 
 export function editNode(node, context, view) {
-    promt(context.translate("Edit image"), context, node.attrs, view, node)
+    promt(context.translate("Edit image"), context, node.attrs, view, node);
 }
 
-let isDefined = function(obj) {
+let isDefined = function (obj) {
     if (arguments.length > 1) {
         let result = true;
-        this.each(arguments, function(index, value) {
+        this.each(arguments, function (index, value) {
             if (!isDefined(value)) {
                 return false;
             }
@@ -45,7 +45,7 @@ let isDefined = function(obj) {
     return typeof obj !== 'undefined';
 };
 
-let endsWith = function(val, suffix) {
+let endsWith = function (val, suffix) {
     if (!isDefined(val) || !isDefined(suffix)) {
         return false;
     }
@@ -56,7 +56,7 @@ export function promt(title, context, attrs, view, node) {
     let state = view.state;
     let {from, to} = state.selection;
 
-    let cleanDimension = function(val) {
+    let cleanDimension = function (val) {
         val = val.trim();
         if (endsWith(val, 'px')) {
             val = val.substring(0, val.length - 2);
@@ -64,17 +64,17 @@ export function promt(title, context, attrs, view, node) {
         return val;
     };
 
-    let validateDimension = function(val) {
+    let validateDimension = function (val) {
         val = cleanDimension(val);
 
         if (val.length && !/^[0-9]+%?$/.test(val)) {
-            return context.translate('Invalid dimension format used.')
+            return context.translate('Invalid dimension format used.');
         }
     };
 
-    let validateSource = function(val) {
+    let validateSource = function (val) {
         if (!validateHref(val)) {
-            return context.translate('Invalid image source.')
+            return context.translate('Invalid image source.');
         }
     };
 
@@ -125,16 +125,14 @@ export function promt(title, context, attrs, view, node) {
             view.dispatch(view.state.tr.replaceSelectionWith(context.schema.nodes.image.createAndFill(attrs)));
             view.focus()
         }
-    })
+    });
 }
 
 export function menu(context) {
-    return [
-        {
-            id: 'insertImage',
-            node: 'image',
-            group: 'insert',
-            item: insertImageItem(context)
-        }
-    ]
+    return [{
+        id: 'insertImage',
+        node: 'image',
+        group: 'insert',
+        item: insertImageItem(context)
+    }];
 }
